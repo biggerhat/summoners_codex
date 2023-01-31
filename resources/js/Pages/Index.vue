387a1a5eb54c390c3938f2ca9d9dc9292021545f
Mark Czirr <shutdownemergency@gmail.com>
2023-01-31 10:59:30 -0500

    add primevue

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <Head title="Home" />
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            Hello
+                <Link href="/test" class="mx-2">
+                    Test
+                </Link>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
