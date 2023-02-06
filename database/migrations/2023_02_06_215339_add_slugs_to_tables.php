13971261966f7d79ccd24889ee8955976a0b1676
Mark Czirr <shutdownemergency@gmail.com>
2023-02-06 17:14:43 -0500

    add slugs and model changes

---
+++ database/migrations/2023_02_06_215339_add_slugs_to_tables.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sets', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('attributes', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('characters', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('cards', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('status', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('teams', function (Blueprint $table) {
+            $table->string("slug");
+        });
+
+        Schema::table('card_types', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+
+        Schema::table('card_phases', function (Blueprint $table) {
+            $table->string("slug")->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tables', function (Blueprint $table) {
+            //
+        });
+    }
+};
